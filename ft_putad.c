ec5cca2b3aa6fc0963af085289b945311c28fd0f
Ajjig <ajjigsimo@gmail.com>
2021-11-18 21:41:48 +0100

    fix putadress function errors

--- ft_putad.c
+++ ft_putad.c
@@ -6,20 +6,20 @@
 /*   By: majjig <majjig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 04:08:32 by majjig            #+#    #+#             */
-/*   Updated: 2021/11/18 20:03:09 by majjig           ###   ########.fr       */
+/*   Updated: 2021/11/18 21:41:06 by majjig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_putlong(unsigned long nb, int *count)
+void	ft_putlong(unsigned long int nb, int *count)
 {
-	if (nb > 16)
+	if (nb >= 16)
 		ft_putlong(nb / 16, count);
-	count += write(1, &"0123456789abcdef"[nb % 16], 1);
+	*count += write(1, &"0123456789abcdef"[nb % 16], 1);
 }
 
-void	ft_putad(unsigned long nb, int *count)
+void	ft_putad(unsigned long int nb, int *count)
 {
 	*count += write(1, "0x", 2);
 	ft_putlong(nb, count);
