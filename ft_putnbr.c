f09fedab3860ae3144d7a49cb361ba115f74241b
Ajjig <ajjigsimo@gmail.com>
2021-11-18 21:19:55 +0100

    fixing negative ints error

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: majjig <majjig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 23:34:59 by majjig            #+#    #+#             */
-/*   Updated: 2021/11/18 04:14:16 by majjig           ###   ########.fr       */
+/*   Updated: 2021/11/18 21:18:44 by majjig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,11 @@
 void	ft_putnbr(int nb, char base, int *count)
 {
 	if (nb < 0)
-		*count += (nb = -nb * write(1, "-", 1));
-	if (nb > BASE)
+	{
+		*count += write(1, "-", 1);
+		nb *= -1;
+	}
+	if (nb >= BASE)
 		ft_putnbr(nb / BASE, base, count);
 	if (base == 'x')
 		*count += write(1, &"0123456789abcdef"[nb % 16], 1);
