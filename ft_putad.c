4e2ce5a3c7321f846f99f0b00db5e63ca0b3aee1
Ajjig <ajjigsimo@gmail.com>
2021-11-18 21:25:09 +0100

    fixed putlong hexadecimal error

--- ft_putad.c
+++ ft_putad.c
@@ -6,7 +6,7 @@
 /*   By: majjig <majjig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 04:08:32 by majjig            #+#    #+#             */
-/*   Updated: 2021/11/18 04:17:44 by majjig           ###   ########.fr       */
+/*   Updated: 2021/11/18 20:03:09 by majjig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	ft_putlong(unsigned long nb, int *count)
 {
-	if (nb > 10)
-		ft_putlong(nb / 10, count);
-	count += write(1, &"0123456789"[nb % 10], 1);
+	if (nb > 16)
+		ft_putlong(nb / 16, count);
+	count += write(1, &"0123456789abcdef"[nb % 16], 1);
 }
 
 void	ft_putad(unsigned long nb, int *count)
 {
-	*count += write(1, "0x", 1);
+	*count += write(1, "0x", 2);
 	ft_putlong(nb, count);
 }
