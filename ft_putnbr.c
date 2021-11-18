ad7bb956b30762f9263310fa1b30852500a6f06f
Ajjig <ajjigsimo@gmail.com>
2021-11-18 04:15:55 +0100

    fixed

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: majjig <majjig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 23:34:59 by majjig            #+#    #+#             */
-/*   Updated: 2021/11/18 03:56:15 by majjig           ###   ########.fr       */
+/*   Updated: 2021/11/18 04:14:16 by majjig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_putnbr(int nb, char base, int *count)
 {
 	if (nb < 0)
 		*count += (nb = -nb * write(1, "-", 1));
-	if (nb >= BASE)
+	if (nb > BASE)
 		ft_putnbr(nb / BASE, base, count);
 	if (base == 'x')
 		*count += write(1, &"0123456789abcdef"[nb % 16], 1);
--- ft_putunsigned.c
+++ ft_putunsigned.c
@@ -6,7 +6,7 @@
 /*   By: majjig <majjig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 03:27:07 by majjig            #+#    #+#             */
-/*   Updated: 2021/11/18 03:48:46 by majjig           ###   ########.fr       */
+/*   Updated: 2021/11/18 04:14:36 by majjig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_putunsigned(unsigned int nb, int *count)
 {
-	if (nb >= 10)
+	if (nb > 10)
 		ft_putunsigned(nb / 10, count);
 	*count += write(1, &"0123456789"[nb % 10],1);
 }
