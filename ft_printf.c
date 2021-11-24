415152743ad59a90f37a6aeabf69e339b5c3b939
Ajjig <ajjigsimo@gmail.com>
2021-11-24 20:35:17 +0100

    added new macros to m/b printf function

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: majjig <majjig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/21 04:57:29 by majjig            #+#    #+#             */
-/*   Updated: 2021/11/24 20:02:55 by majjig           ###   ########.fr       */
+/*   Updated: 2021/11/24 20:23:35 by majjig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 void	ft_format_detect(char flag, va_list ap, int *count)
 {
-	if (flag == 'c')
+	if (flag == CHAR)
 		ft_putchar(va_arg(ap, int), count);
-	else if (flag == 's')
+	else if (flag == STRING)
 		ft_putstr(va_arg(ap, char *), count);
-	else if (flag == 'i' || flag == 'd')
+	else if (flag == INTEGER || flag == DECIMAL)
 		ft_putnbr(va_arg(ap, int), count);
-	else if (flag == 'u' || flag == 'X' || flag == 'x')
+	else if (flag == UNSIGNED || flag == HEXAUPPER || flag == HEXALOWER)
 		ft_putunsigned(va_arg(ap, unsigned int), flag, count);
-	else if (flag == 'p')
+	else if (flag == ADDRESS)
 		ft_putad(va_arg(ap, unsigned long int), count);
 	else
 		*count += write(1, &flag, 1);
@@ -37,11 +37,11 @@ int	ft_printf(const char *s, ...)
 	count = 0;
 	i = -1;
 	if (s == NULL)
-		return (-1);
+		return (EXIT_ERROR);
 	va_start(ap, s);
 	while (s[++i])
 	{
-		if (s[i] == '%')
+		if (s[i] == PERCENT)
 			ft_format_detect(s[++i], ap, &count);
 		else
 			count += write(1, s + i, 1);
