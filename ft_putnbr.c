64d7bab6c522240fc21a6480cb92b47d636c4586
Ajjig <ajjigsimo@gmail.com>
2021-11-24 20:33:23 +0100

    strdecimal macro

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: majjig <majjig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 23:34:59 by majjig            #+#    #+#             */
-/*   Updated: 2021/11/18 22:13:31 by majjig           ###   ########.fr       */
+/*   Updated: 2021/11/24 20:28:34 by majjig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,5 @@ void	ft_putnbr(int nb, int *count)
 	}
 	if (nb >= 10)
 		ft_putnbr(nb / 10, count);
-	*count += write(1, &"0123456789"[nb % 10], 1);
+	*count += write(1, &STR_DECIMAL[nb % 10], 1);
 }
